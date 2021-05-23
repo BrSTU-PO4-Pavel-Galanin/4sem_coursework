ca41fc8c24c2270b25a9d31a573828cd68eed34d
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2021-05-24 01:15:07 +0300

    coursework/Galanin_Pavel : Updated src for no GPU

--- src/celsii_to_farengate.py
+++ src/celsii_to_farengate.py
@@ -1,21 +1,40 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow import keras
-from tensorflow.keras.layers import Dense
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #ignore
 
-c = np.array([-40, -10, 0, 8, 15, 22, 38])
-f = np.array([-40, 14, 32, 46, 59, 72, 100])
+import numpy
+import matplotlib.pyplot
+import tensorflow
 
-model = keras.Sequential()
-model.add(Dense(units=1, input_shape=(1,), activation='linear'))
-model.compile(loss='mean_squared_error', optimizer=keras.optimizers.Adam(0.1))
+c = numpy.array([-40, -10, 0, 8, 15, 22, 38])
+f = numpy.array([-40, 14, 32, 46, 59, 72, 100])
 
-history = model.fit(c, f, epochs=500, verbose=0)
+model = tensorflow.keras.Sequential()
+
+model.add(
+    tensorflow.keras.layers.Dense(
+        units=1,
+        input_shape=(1,),
+        activation='linear'
+    )
+)
+
+model.compile(
+    loss='mean_squared_error',
+    optimizer=tensorflow.keras.optimizers.Adam(0.1)
+)
+
+history = model.fit(
+    c,
+    f,
+    epochs=500,
+    verbose=0
+)
+
 print("Обучение завершено")
 
-print(model.predict([100]))
-print(model.get_weights())
+print( model.predict([100, 101, 102]) )
+print( model.get_weights() )
 
-plt.plot(history.history['loss'])
-plt.grid(True)
-plt.show()
+matplotlib.pyplot.plot(history.history['loss'])
+matplotlib.pyplot.grid(True)
+matplotlib.pyplot.show()
