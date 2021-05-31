e6ec0261fb48905740a9ca000ec51d735cc1cf24
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-05-31 01:57:21 -0700

    coursework/Galanin_Pavel : Nesting libs

--- src/catsVsDogs/main.py
+++ src/catsVsDogs/main.py
@@ -1,14 +1,21 @@
-import numpy as np # массив
-import tensorflow as tf # tensorflow
-import tensorflow_datasets as tfds # mnist
-from tensorflow.keras.preprocessing.image import load_img, img_to_array # загрузка изображения, изображение в массив
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout # для создания слоёв
-import matplotlib.pyplot as plt # для графиков
-from google.colab import files # для загрузки файлов
+# = = = = = библиотеки = = = = =
 
-train, _ = tfds.load(
+# При запуске программы TensorFlow 2+ пытается запустить GPU
+# Нам нужен CUDA для GPU TensorFlow
+# Чтобы не выводились предупреждения при запуске программы
+# пропишем две строчки:
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #ignore
+
+import numpy
+import tensorflow
+import tensorflow_datasets
+import matplotlib.pyplot as plt 
+#from google.colab import files # для загрузки файлов в Google Colabs
+
+train, _ = tensorflow_datasets.load(
     'cats_vs_dogs',
-    split=['train[:100%]'],
+    split=['train[:1%]'],
     with_info=True,
     as_supervised=True
 )
@@ -21,28 +28,33 @@ for img, label in train[0].take(10):
 
 SIZE = 224
 def resize_image(img, label):
-    img = tf.cast(img, tf.float32)
-    img = tf.image.resize(img, (SIZE, SIZE))
+    img = tensorflow.cast(img, tensorflow.float32)
+    img = tensorflow.image.resize(img, (SIZE, SIZE))
     img = img / 255.0
     return img, label
 
 train_resized = train[0].map(resize_image)
 train_batches = train_resized.shuffle(1000).batch(16)
 
-base_layers = tf.keras.applications.MobileNetV2(input_shape=(SIZE, SIZE, 3), include_top=False)
+base_layers = tensorflow.keras.applications.MobileNetV2(input_shape=(SIZE, SIZE, 3), include_top=False)
 base_layers.trainable = False
 
-model = tf.keras.Sequential([
+model = tensorflow.keras.Sequential([
     base_layers,
-    GlobalAveragePooling2D(),
-    Dropout(0.2),
-    Dense(1)
+    tensorflow.keras.layers.GlobalAveragePooling2D(),
+    tensorflow.keras.layers.Dropout(0.2),
+    tensorflow.keras.layers.Dense(1)
 ])
-model.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=['accuracy'])
+model.compile(
+    optimizer='adam',
+    loss=tensorflow.keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=['accuracy']
+)
 
 model.fit(train_batches, epochs=1)
 
-f = files.upload()
+f = []
+#f = files.upload()
 
 list_img_path = []
 for key in f:
@@ -50,10 +62,10 @@ for key in f:
 
 for i in range(len(list_img_path)):
     path = list_img_path[i]
-    img = load_img(path)
-    img_array = img_to_array(img)
+    img = tensorflow.keras.preprocessing.imag.load_img(path)
+    img_array = tensorflow.keras.preprocessing.imag.img_to_array(img)
     img_resized, _ = resize_image(img_array, _)
-    img_expended = np.expand_dims(img_resized, axis=0)
+    img_expended = numpy.expand_dims(img_resized, axis=0)
     prediction = model.predict(img_expended)[0][0]
     pred_label = 'Cat' if prediction < 0.5 else 'Dog'
     plt.figure()
@@ -64,10 +76,10 @@ model.fit(train_batches, epochs=1)
 
 for i in range(len(list_img_path)):
     path = list_img_path[i]
-    img = load_img(path)
-    img_array = img_to_array(img)
+    img = tensorflow.keras.preprocessing.imag.load_img(path)
+    img_array = tensorflow.keras.preprocessing.imag.img_to_array(img)
     img_resized, _ = resize_image(img_array, _)
-    img_expended = np.expand_dims(img_resized, axis=0)
+    img_expended = numpy.expand_dims(img_resized, axis=0)
     prediction = model.predict(img_expended)[0][0]
     pred_label = 'Cat' if prediction < 0.5 else 'Dog'
     plt.figure()
@@ -78,10 +90,10 @@ model.fit(train_batches, epochs=1)
 
 for i in range(len(list_img_path)):
     path = list_img_path[i]
-    img = load_img(path)
-    img_array = img_to_array(img)
+    img = tensorflow.keras.preprocessing.imag.load_img(path)
+    img_array = tensorflow.keras.preprocessing.imag.img_to_array(img)
     img_resized, _ = resize_image(img_array, _)
-    img_expended = np.expand_dims(img_resized, axis=0)
+    img_expended = numpy.expand_dims(img_resized, axis=0)
     prediction = model.predict(img_expended)[0][0]
     pred_label = 'Cat' if prediction < 0.5 else 'Dog'
     plt.figure()
